1d509011bd8a0dfb7f5a6559298be79f71640609
Tobias Parent <parent.tobias@gmail.com>
2013-04-26 12:58:24 -0700

    First rewrite: added PHP-populated variables."

--- index.php
+++ index.php
@@ -50,24 +50,38 @@ if ($rsp_obj['stat'] == 'ok'){
 <!DOCTYPE html>
 <html lang="en/us">
   <head>
-    <title>Flickr Feed Tutorial--first draft</title>
+    <title>Flickr Feed Tutorial--second draft</title>
+    <script type="text/javascript" src="./assets/js/vendor/jquery-1.9.1.js" ></script>
+    <script type="text/javascript" src="./assets/js/vendor/underscore.js"></script>
     <link rel="stylesheet" href="./assets/css/style.css" />
+    
+    <script type="text/javascript">
+      var flickrUrlRoot = "http://api.flickr.com/services/rest/?jsoncallback=?",
+          totalPages = <? echo $rsp_obj['photos']['pages']; ?>,
+          flickrConfig = {
+            api_key:    "<? echo $params['api_key'] ?>",
+            method:     "<?echo $params['method'] ?>",
+            format:     "json",
+            per_page:   "<? echo $params['per-page'] ?>",
+            page:       "<? echo $rsp_obj['photos']['page']+1; ?>"
+          };
+    </script>
   </head>
   <body>
     <hgroup>
-      <h1>Tutorial, First revision</h1>
-      <h2>Each click of next/previous re-renders everything</h2>
+      <h1>Tutorial, Second revision</h1>
+      <h2>Clicking "More..." will call the API and append more images</h2>
     </hgroup>
-    <p>In this, the first draft of a evolution series, a set of images are being fetched from flickr.com, their 'interestingness' collection. It's being fetched in blocks of ten, and clicking on previous/next below results in a call to PHP, which fetches the entire page, showing the next/previous ten images.</p>
+    <p>In this revision, the page is initially returned with a set of populated images by PHP, but all subsequent calls are done via AJAX.</p>
     <section class="pros">
       <h1>Pros</h1>
-      <p>This is the traditional model, call-and-response. Works fine, and allows the back end to have more control over what the front end can access. Really can't think of many advantages to this.</p>
+      <p>Far less data is being passed, and the page isn't being re-rendered every time, which should speed things up considerably. Also, because the AJAX call is going to a remote server (rather than mine), server load is decreased.</p>
     </section>
     <section class="cons">
       <h1>Cons</h1>
-      <p>Server load, to begin with -- while this is a small example, and it isn't storing data on my server, it calls flickr, parses the feed and outputs the data, which does result in a server hit each time.</p>
-      <p>Non-intuitive to modern users. Rather than re-rendering everything, we could simply let javascript do the work and get more images. Like, for example, <a href="../step_02/">this</a>.</p>
-    </section>    <div class="image-listing">
+      <p>...</p>
+    </section>
+    <div class="image-listing">
     <?php foreach( $rsp_obj['photos']['photo'] as $photo){ ?>
       <img src='http://farm<? echo $photo["farm"]; ?>.staticflickr.com/<? echo $photo["server"]; ?>/<? echo $photo["id"]; ?>_<? echo $photo["secret"] ?>_s.jpg' />
     <?php }; ?>
