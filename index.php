5d96d203c0a05afb55dff469168f74504c17530d
Tobias Parent <parent.tobias@gmail.com>
2013-05-07 12:28:36 -0700

    Third draft, JS Only

--- index.php
+++ index.php
@@ -1,78 +1,39 @@
-<?php
- /********
-  * Flickr.com feed tutorial, step 1.
-  * v 0.0.1, 04/23/2013 -- Tobias Parent (http://snowmonkey.koding.com/)
-  * 
-  *******/
 
-#
-# build the API URL to call
-#
-if($_GET['pageno']){
-  $pageNo = $_GET['pageno'];
-} else {
-  $pageNo = 1;
-};
-
-$params = array(
-  'api_key'	 => '[YOUR API KEY GOES HERE]',
-  'method'	 => 'flickr.interestingness.getList',
-  'format'	 => 'php_serial',
-  'per_page' => '10',
-  'page'     => $pageNo,
-);
-
-$encoded_params = array();
-
-foreach ($params as $k => $v){
-
-	$encoded_params[] = urlencode($k).'='.urlencode($v);
-}
-
-
-#
-# call the API and decode the response
-#
-
-$url = "http://api.flickr.com/services/rest/?".implode('&', $encoded_params);
-
-$rsp = file_get_contents($url);
-
-$rsp_obj = unserialize($rsp);
-
-
-#
-# display the photo title (or an error if it failed)
-#
-
-if ($rsp_obj['stat'] == 'ok'){
-?>
 <!DOCTYPE html>
 <html lang="en/us">
   <head>
-    <title>Flickr Feed Tutorial--second draft</title>
+    <title>Flickr Feed Tutorial--third draft</title>
     <script type="text/javascript" src="./assets/js/vendor/jquery-1.9.1.js" ></script>
     <script type="text/javascript" src="./assets/js/vendor/underscore.js"></script>
     <link rel="stylesheet" href="./assets/css/style.css" />
     
     <script type="text/javascript">
       var flickrUrlRoot = "http://api.flickr.com/services/rest/?jsoncallback=?",
-          totalPages = <? echo $rsp_obj['photos']['pages']; ?>,
+          totalPages,
           flickrConfig = {
-            api_key:    "<? echo $params['api_key'] ?>",
-            method:     "<?echo $params['method'] ?>",
+            api_key:    "[YOUR API KEY GOES HERE],
+            method:     "flickr.interestingness.getList,
             format:     "json",
-            per_page:   "<? echo $params['per_page'] ?>",
-            page:       "<? echo $rsp_obj['photos']['page']+1; ?>"
+            per_page:   "10",
+            page:       1
           };
           
       // When the document has loaded, we can attach the listener to the button,
       //  and set its processing
       $(document).ready(function(){
-        var imagePane = $('.image-listing');
+        fetchMorePhotos();
         $('.next-set').on("click", function(){
-          // When the 'Get more photos' button is clicked, fetch the JSON feed,
-          //   parse it, and populate the image tags.
+          fetchMorePhotos();
+        });
+        
+        function fetchMorePhotos(){
+          // Moving the 'Fetch more photos' routine out of the event handler, as it will
+          //   need to be called in more places: we'll call it on init, and on the click.
+          //   In both cases, the functionality will be identical: fetch the url with the
+          //   config object, append the images to the pane, update the config object.
+
+          var imagePane = $('.image-listing');
+
           $.getJSON(flickrUrlRoot, flickrConfig, function(data){
             // If we're here, we have a dataset. Parse it and populate.
             _.each(data.photos.photo, function(photo){
@@ -87,7 +48,7 @@ if ($rsp_obj['stat'] == 'ok'){
             // And also update the page number that will be passed to the API
             flickrConfig.page = Number(flickrConfig.page)+1;
           })
-        });
+        }
       
       });
     </script>
@@ -107,9 +68,7 @@ if ($rsp_obj['stat'] == 'ok'){
       <p>...</p>
     </section>
     <div class="image-listing">
-    <?php foreach( $rsp_obj['photos']['photo'] as $photo){ ?>
-      <img src='http://farm<? echo $photo["farm"]; ?>.staticflickr.com/<? echo $photo["server"]; ?>/<? echo $photo["id"]; ?>_<? echo $photo["secret"] ?>_s.jpg' />
-    <?php }; ?>
+
     </div>
     <nav>
       <span class="page-count-status">Page <? echo $rsp_obj['photos']['page']; ?> of <? echo $rsp_obj['photos']['pages']; ?> 
@@ -119,10 +78,3 @@ if ($rsp_obj['stat'] == 'ok'){
     </nav>
   </body>
 </html>
-<?
-}else{
-
-	echo "Call failed!";
-}
-
-?>
